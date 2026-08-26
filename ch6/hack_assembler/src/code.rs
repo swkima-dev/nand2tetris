e878fc791091a78ac8cf0f25ba6073c98bb84b20
swkima-popos <greyonth@gmail.com>
2026-08-26 13:21:56 +0900

    refactor: symbols init

--- ch6/hack_assembler/src/code.rs
+++ ch6/hack_assembler/src/code.rs
@@ -12,31 +12,7 @@ impl Code {
     pub fn new() -> Self {
         let mut symbols: HashMap<String, u16> = HashMap::new();
 
-        symbols.insert("R0".to_string(), 0);
-        symbols.insert("R1".to_string(), 1);
-        symbols.insert("R2".to_string(), 2);
-        symbols.insert("R3".to_string(), 3);
-        symbols.insert("R4".to_string(), 4);
-        symbols.insert("R5".to_string(), 5);
-        symbols.insert("R6".to_string(), 6);
-        symbols.insert("R7".to_string(), 7);
-        symbols.insert("R8".to_string(), 8);
-        symbols.insert("R9".to_string(), 9);
-        symbols.insert("R10".to_string(), 10);
-        symbols.insert("R11".to_string(), 11);
-        symbols.insert("R12".to_string(), 12);
-        symbols.insert("R13".to_string(), 13);
-        symbols.insert("R14".to_string(), 14);
-        symbols.insert("R15".to_string(), 15);
-
-        symbols.insert("SP".to_string(), 0);
-        symbols.insert("LCL".to_string(), 1);
-        symbols.insert("ARG".to_string(), 2);
-        symbols.insert("THIS".to_string(), 3);
-        symbols.insert("THAT".to_string(), 4);
-
-        symbols.insert("SCREEN".to_string(), 16384);
-        symbols.insert("KBD".to_string(), 24576);
+        Self::symbols_init(&mut symbols);
 
         Self {
             symbols,
@@ -92,6 +68,34 @@ impl Code {
             }
             _ => Ok(None),
         }
+    }
+
+    fn symbols_init(symbols: &mut HashMap<String, u16>) {
+        symbols.insert("R0".to_string(), 0);
+        symbols.insert("R1".to_string(), 1);
+        symbols.insert("R2".to_string(), 2);
+        symbols.insert("R3".to_string(), 3);
+        symbols.insert("R4".to_string(), 4);
+        symbols.insert("R5".to_string(), 5);
+        symbols.insert("R6".to_string(), 6);
+        symbols.insert("R7".to_string(), 7);
+        symbols.insert("R8".to_string(), 8);
+        symbols.insert("R9".to_string(), 9);
+        symbols.insert("R10".to_string(), 10);
+        symbols.insert("R11".to_string(), 11);
+        symbols.insert("R12".to_string(), 12);
+        symbols.insert("R13".to_string(), 13);
+        symbols.insert("R14".to_string(), 14);
+        symbols.insert("R15".to_string(), 15);
+
+        symbols.insert("SP".to_string(), 0);
+        symbols.insert("LCL".to_string(), 1);
+        symbols.insert("ARG".to_string(), 2);
+        symbols.insert("THIS".to_string(), 3);
+        symbols.insert("THAT".to_string(), 4);
+
+        symbols.insert("SCREEN".to_string(), 16384);
+        symbols.insert("KBD".to_string(), 24576);
     }
 
     fn dest_assemble(dest: &Option<String>) -> u16 {
